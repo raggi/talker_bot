664b27c89932a2f4bade40c8196ad9d4471565c6
raggi <jftucker@gmail.com>
2009-11-13 00:19:30 +0000

    Initial implementation idea

---
+++ Rakefile
@@ -0,0 +1,12 @@
+# -*- ruby -*-
+
+require 'rubygems'
+require 'hoe'
+
+Hoe.spec 'talker_bot' do
+  # developer('FIX', 'FIX@example.com')
+
+  # self.rubyforge_name = 'talker_botx' # if different than 'talker_bot'
+end
+
+# vim: syntax=ruby
---
+++ lib/talker_bot.rb
@@ -0,0 +1,59 @@
+require 'talker'
+require 'talker/cli'
+
+class TalkerBot
+  VERSION = '1.0.0'
+  TALKER_OPTS = [:host, :port, :room, :token]
+  TALKER_CALLBACKS = Talker.instance_methods.grep(/^on_/).map { |m| m.to_sym }
+  UNDERSCORE = '_'
+
+  def self.start(config = {}, &block)
+    bot = new(config)
+    bot.instance_eval(&block) if block
+    bot.connect
+    bot.run
+  end
+
+  def initialize(config = {})
+    @config = config
+    config[:token] ||= Talker::CLI.load_token
+    @plugins = []
+    @plugin_callbacks = Hash.new { |h,k| h[k] = [] }
+  end
+
+  def config(name, value = nil)
+    if value
+      @config[name] = value
+    else
+      @config[name]
+    end
+  end
+
+  def plugin(plugin)
+    @plugins << plugin
+    TALKER_CALLBACKS.each do |callback|
+      @plugin_callbacks[callback] << plugin if plugin.respond_to?(callback)
+    end
+  end
+  alias use plugin
+
+  def connect
+    Talker.connect(@config) do |client|
+      TALKER_CALLBACKS.each do |cb|
+        client.__send__(cb) { |*args| event(cb, *args) }
+      end
+      @plugins.each { |p| p.client = client if p.respond_to?(:client=)}
+    end
+  end
+  alias run connect
+
+  def event(type, *args)
+    @plugin_callbacks[type].each { |plugin| plugin.__send__(type, *args) }
+  end
+
+  def run_filter_actions(list, *args)
+    list.each do |filter, action|
+      action.call(*args) if filter === event
+    end
+  end
+end
